1292f72f5f5600a69dd6525f992e843b106ce10c
David <david.fiala@post.cz>
2025-10-25 07:40:55 +0000

    init

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import './bootstrap';
+import { createApp } from 'vue';
+
+// Create a minimal Vue app. Mounts to an element with id="app" if present in your Blade views.
+const app = createApp({});
+app.mount('#app');
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.js',
+    './resources/js/**/*.vue',
+    './resources/**/*.blade.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
